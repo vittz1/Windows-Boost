c19bd5cfa6e3b2a8d4be807b7141dd4163bd380d
Joao Vitor <62403457+vittz1@users.noreply.github.com>
2021-03-18 11:13:28 -0300

    Add files via upload

---
+++ Vittz Boost/Vittz Boost/Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits MetroFramework.Forms.MetroForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MetroButton1 = New MetroFramework.Controls.MetroButton()
+        Me.MetroButton2 = New MetroFramework.Controls.MetroButton()
+        Me.MetroButton3 = New MetroFramework.Controls.MetroButton()
+        Me.MetroButton4 = New MetroFramework.Controls.MetroButton()
+        Me.MetroButton5 = New MetroFramework.Controls.MetroButton()
+        Me.MetroButton6 = New MetroFramework.Controls.MetroButton()
+        Me.MetroButton7 = New MetroFramework.Controls.MetroButton()
+        Me.MetroButton8 = New MetroFramework.Controls.MetroButton()
+        Me.MetroButton9 = New MetroFramework.Controls.MetroButton()
+        Me.MetroButton10 = New MetroFramework.Controls.MetroButton()
+        Me.MetroButton11 = New MetroFramework.Controls.MetroButton()
+        Me.SuspendLayout()
+        '
+        'MetroButton1
+        '
+        Me.MetroButton1.Location = New System.Drawing.Point(93, 73)
+        Me.MetroButton1.Name = "MetroButton1"
+        Me.MetroButton1.Size = New System.Drawing.Size(129, 23)
+        Me.MetroButton1.Style = MetroFramework.MetroColorStyle.Black
+        Me.MetroButton1.TabIndex = 0
+        Me.MetroButton1.Text = "Cleaner Cache"
+        Me.MetroButton1.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroButton1.UseSelectable = True
+        '
+        'MetroButton2
+        '
+        Me.MetroButton2.Location = New System.Drawing.Point(93, 102)
+        Me.MetroButton2.Name = "MetroButton2"
+        Me.MetroButton2.Size = New System.Drawing.Size(129, 23)
+        Me.MetroButton2.Style = MetroFramework.MetroColorStyle.Black
+        Me.MetroButton2.TabIndex = 1
+        Me.MetroButton2.Text = "Disable Hibernation"
+        Me.MetroButton2.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroButton2.UseSelectable = True
+        '
+        'MetroButton3
+        '
+        Me.MetroButton3.Location = New System.Drawing.Point(93, 131)
+        Me.MetroButton3.Name = "MetroButton3"
+        Me.MetroButton3.Size = New System.Drawing.Size(129, 23)
+        Me.MetroButton3.Style = MetroFramework.MetroColorStyle.Black
+        Me.MetroButton3.TabIndex = 2
+        Me.MetroButton3.Text = "Registry Optimizations"
+        Me.MetroButton3.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroButton3.UseSelectable = True
+        '
+        'MetroButton4
+        '
+        Me.MetroButton4.Location = New System.Drawing.Point(93, 160)
+        Me.MetroButton4.Name = "MetroButton4"
+        Me.MetroButton4.Size = New System.Drawing.Size(129, 23)
+        Me.MetroButton4.Style = MetroFramework.MetroColorStyle.Black
+        Me.MetroButton4.TabIndex = 3
+        Me.MetroButton4.Text = "DNS Cleaner"
+        Me.MetroButton4.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroButton4.UseSelectable = True
+        '
+        'MetroButton5
+        '
+        Me.MetroButton5.Location = New System.Drawing.Point(93, 189)
+        Me.MetroButton5.Name = "MetroButton5"
+        Me.MetroButton5.Size = New System.Drawing.Size(129, 23)
+        Me.MetroButton5.Style = MetroFramework.MetroColorStyle.Black
+        Me.MetroButton5.TabIndex = 4
+        Me.MetroButton5.Text = "Repair HD"
+        Me.MetroButton5.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroButton5.UseSelectable = True
+        '
+        'MetroButton6
+        '
+        Me.MetroButton6.Location = New System.Drawing.Point(93, 218)
+        Me.MetroButton6.Name = "MetroButton6"
+        Me.MetroButton6.Size = New System.Drawing.Size(129, 23)
+        Me.MetroButton6.Style = MetroFramework.MetroColorStyle.Black
+        Me.MetroButton6.TabIndex = 5
+        Me.MetroButton6.Text = "Ultimate Energy Plan"
+        Me.MetroButton6.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroButton6.UseSelectable = True
+        '
+        'MetroButton7
+        '
+        Me.MetroButton7.Location = New System.Drawing.Point(93, 247)
+        Me.MetroButton7.Name = "MetroButton7"
+        Me.MetroButton7.Size = New System.Drawing.Size(129, 23)
+        Me.MetroButton7.Style = MetroFramework.MetroColorStyle.Black
+        Me.MetroButton7.TabIndex = 6
+        Me.MetroButton7.Text = "Disable DVR 1"
+        Me.MetroButton7.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroButton7.UseSelectable = True
+        '
+        'MetroButton8
+        '
+        Me.MetroButton8.Location = New System.Drawing.Point(93, 276)
+        Me.MetroButton8.Name = "MetroButton8"
+        Me.MetroButton8.Size = New System.Drawing.Size(129, 23)
+        Me.MetroButton8.Style = MetroFramework.MetroColorStyle.Black
+        Me.MetroButton8.TabIndex = 7
+        Me.MetroButton8.Text = "Disable DVR 2"
+        Me.MetroButton8.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroButton8.UseSelectable = True
+        '
+        'MetroButton9
+        '
+        Me.MetroButton9.Location = New System.Drawing.Point(93, 305)
+        Me.MetroButton9.Name = "MetroButton9"
+        Me.MetroButton9.Size = New System.Drawing.Size(129, 23)
+        Me.MetroButton9.Style = MetroFramework.MetroColorStyle.Black
+        Me.MetroButton9.TabIndex = 8
+        Me.MetroButton9.Text = "Fullscream FIX"
+        Me.MetroButton9.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroButton9.UseSelectable = True
+        '
+        'MetroButton10
+        '
+        Me.MetroButton10.Location = New System.Drawing.Point(93, 334)
+        Me.MetroButton10.Name = "MetroButton10"
+        Me.MetroButton10.Size = New System.Drawing.Size(129, 23)
+        Me.MetroButton10.Style = MetroFramework.MetroColorStyle.Black
+        Me.MetroButton10.TabIndex = 9
+        Me.MetroButton10.Text = "Disable WDefender"
+        Me.MetroButton10.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroButton10.UseSelectable = True
+        '
+        'MetroButton11
+        '
+        Me.MetroButton11.Location = New System.Drawing.Point(93, 363)
+        Me.MetroButton11.Name = "MetroButton11"
+        Me.MetroButton11.Size = New System.Drawing.Size(129, 23)
+        Me.MetroButton11.Style = MetroFramework.MetroColorStyle.Black
+        Me.MetroButton11.TabIndex = 10
+        Me.MetroButton11.Text = "Disable APPs"
+        Me.MetroButton11.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.MetroButton11.UseSelectable = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(312, 402)
+        Me.Controls.Add(Me.MetroButton11)
+        Me.Controls.Add(Me.MetroButton10)
+        Me.Controls.Add(Me.MetroButton9)
+        Me.Controls.Add(Me.MetroButton8)
+        Me.Controls.Add(Me.MetroButton7)
+        Me.Controls.Add(Me.MetroButton6)
+        Me.Controls.Add(Me.MetroButton5)
+        Me.Controls.Add(Me.MetroButton4)
+        Me.Controls.Add(Me.MetroButton3)
+        Me.Controls.Add(Me.MetroButton2)
+        Me.Controls.Add(Me.MetroButton1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Resizable = False
+        Me.ShadowType = MetroFramework.Forms.MetroFormShadowType.DropShadow
+        Me.Style = MetroFramework.MetroColorStyle.Red
+        Me.Text = " WINDOWS BOOSTER 1.0"
+        Me.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center
+        Me.Theme = MetroFramework.MetroThemeStyle.Dark
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents MetroButton1 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroButton2 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroButton3 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroButton4 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroButton5 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroButton6 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroButton7 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroButton8 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroButton9 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroButton10 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroButton11 As MetroFramework.Controls.MetroButton
+End Class
---
+++ Vittz Boost/Vittz Boost/Form1.vb
@@ -0,0 +1,102 @@
+﻿Imports System.IO
+Imports System.Security.Principal
+Imports System.Threading
+
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '/// executar somente com privilegio de administrador
+        Dim mPrincipal As WindowsPrincipal = New WindowsPrincipal(WindowsIdentity.GetCurrent)
+        If mPrincipal.IsInRole(WindowsBuiltInRole.Administrator) = False Then
+            MessageBox.Show("Você precisa executar a aplicação como Administrador", "VITTZ BOOST", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Application.Exit()
+        Else
+            MessageBox.Show("Sigam no instagram: @Vithor97", "Administrador", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Process.Start("https://www.instagram.com/vithor97/")
+        End If
+
+    End Sub
+
+    Private Sub MetroButton1_Click(sender As Object, e As EventArgs) Handles MetroButton1.Click
+
+        Dim dir As String = My.Computer.FileSystem.SpecialDirectories.Temp
+        Dim filename As String = dir + "CacheCleaner.bat"
+        IO.File.WriteAllText(filename, My.Resources.CacheCleaner)
+        Process.Start(filename)
+
+
+    End Sub
+
+    Private Sub MetroButton2_Click(sender As Object, e As EventArgs) Handles MetroButton2.Click
+        Process.Start("cmd", "/K powercfg -h off & @echo Hibernation is disabled successfully. ")
+
+    End Sub
+
+    Private Sub MetroButton3_Click(sender As Object, e As EventArgs) Handles MetroButton3.Click
+        Dim dir As String = My.Computer.FileSystem.SpecialDirectories.Temp
+        Dim filename As String = dir + "RegistryOptimizations.reg"
+        IO.File.WriteAllText(filename, My.Resources.RegistryOptimizations)
+        Process.Start(filename)
+    End Sub
+
+    Private Sub MetroButton4_Click(sender As Object, e As EventArgs) Handles MetroButton4.Click
+
+        Process.Start("cmd", "/K ipconfig /flushdns")
+
+    End Sub
+
+    Private Sub MetroButton5_Click(sender As Object, e As EventArgs) Handles MetroButton5.Click
+        Process.Start("cmd", "/K Dism /Online /Cleanup-Image /ScanHealth && Dism /Online /Cleanup-Image /RestoreHealth")
+    End Sub
+
+    Private Sub MetroButton6_Click(sender As Object, e As EventArgs) Handles MetroButton6.Click
+
+        Process.Start("cmd", "/K powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61")
+
+    End Sub
+
+    Private Sub MetroButton7_Click(sender As Object, e As EventArgs) Handles MetroButton7.Click
+        Dim dir As String = My.Computer.FileSystem.SpecialDirectories.Temp
+        Dim filename As String = dir + "Disable_DVR_1.reg"
+        IO.File.WriteAllText(filename, My.Resources.Disable_DVR_1)
+        Process.Start(filename)
+    End Sub
+
+    Private Sub MetroButton8_Click(sender As Object, e As EventArgs) Handles MetroButton8.Click
+        Dim dir As String = My.Computer.FileSystem.SpecialDirectories.Temp
+        Dim filename As String = dir + "Disable_DVR_2.reg"
+        IO.File.WriteAllText(filename, My.Resources.Disable_DVR_2)
+        Process.Start(filename)
+    End Sub
+
+    Private Sub MetroButton9_Click(sender As Object, e As EventArgs) Handles MetroButton9.Click
+        Dim dir As String = My.Computer.FileSystem.SpecialDirectories.Temp
+        Dim filename As String = dir + "fullscreenfix.reg"
+        IO.File.WriteAllText(filename, My.Resources.fullscreenfix)
+        Process.Start(filename)
+    End Sub
+
+    Private Sub MetroButton10_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub MetroButton10_Click_1(sender As Object, e As EventArgs) Handles MetroButton10.Click
+
+        Dim dir As String = My.Computer.FileSystem.SpecialDirectories.Temp
+        Dim filename As String = dir + "DefenderControl.exe"
+        IO.File.WriteAllBytes(filename, My.Resources.DefenderControl)
+        Process.Start(filename)
+
+    End Sub
+
+    Private Sub MetroButton11_Click(sender As Object, e As EventArgs) Handles MetroButton11.Click
+
+        Dim dir As String = My.Computer.FileSystem.SpecialDirectories.Temp
+        Dim filename As String = dir + "_10AppsManager.exe"
+        IO.File.WriteAllBytes(filename, My.Resources._10AppsManager)
+        Process.Start(filename)
+
+    End Sub
+
+End Class
